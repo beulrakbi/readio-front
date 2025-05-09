8b3ba4dd642ba1d7bd369cbf917427d9d42642ec
kangmin1998 <fkdltbtxj1@gmail.comgit config --global user.email fkdltbtxj1@gmail.com>
2025-05-09 16:21:30 +0900

    2025-05-09 공지사항 수정 (마스터 혹시 올라가도 문제 없습니다. 죄송)

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
-import UserMain from "./pages/user/UserMain";
 import PostWriting from "./pages/post/PostWriting";
+import NoticeDetail from './pages/user/NoticeDetail';
+import NoticeList from './pages/user/NoticeList';
+import UserMain from "./pages/user/UserMain";
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<Layout/>}>
           <Route index element={<UserMain/>}/>
           <Route path="post/writing" element={<PostWriting/>}/>
+          <Route path="/notice" element={<NoticeList/>}/>
+          <Route path="/notice/detail" element={<NoticeDetail/>}/>
         </Route>
         </Routes>
       </BrowserRouter>
