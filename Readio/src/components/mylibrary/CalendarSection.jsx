034405feb3c1c4a6b95543e3b79a6e62d6dbcd56
P-sohye <thgp1254@gmail.com>
2025-05-09 12:29:56 +0900

    #7 내서재 메인 페이지 완료

--- Readio/src/components/mylibrary/CalendarSection.jsx
+++ Readio/src/components/mylibrary/CalendarSection.jsx
@@ -8,16 +8,14 @@ const CalendarSection = () => {
     const month = today.month(); // 0~11
 
     const getDaysArray = () => {
-        const startDay = dayjs(new Date(year, month, 1)).day(); // 요일 (0~6)
-        const lastDate = dayjs(new Date(year, month + 1, 0)).date(); // 마지막 일자
+        const startDay = dayjs(new Date(year, month, 1)).day();
+        const lastDate = dayjs(new Date(year, month + 1, 0)).date();
         const days = [];
 
-        // 빈 칸 (1일 전까지)
         for (let i = 0; i < startDay; i++) {
             days.push(null);
         }
 
-        // 실제 날짜
         for (let d = 1; d <= lastDate; d++) {
             days.push(d);
         }
@@ -33,22 +31,23 @@ const CalendarSection = () => {
                 <h2 className={styles.sectionTitle}>{month + 1}월 활동 달력</h2>
                 <span className={styles.sectionAction}>전체보기</span>
             </div>
-            <div className={styles.calendarGrid}>
-                {['일', '월', '화', '수', '목', '금', '토'].map((day, i) => (
-                    <div key={i} className={`${styles.dayHeader}` }>
-                        {day}
-                    </div>
-                ))}
 
-                {days.map((date, idx) => (
-                    <div key={idx} className={styles.dayCell}>
-                        {date || ''}
-                    </div>
-                ))}
+            <div className={styles.calendarWrapper}>
+                <div className={styles.calendarHeaderRow}>
+                    {['일', '월', '화', '수', '목', '금', '토'].map((day, i) => (
+                        <div key={i} className={styles.dayHeader}>{day}</div>
+                    ))}
+                </div>
+
+                <div className={styles.calendarGrid}>
+                    {days.map((date, idx) => (
+                        <div key={idx} className={styles.dayCell}>
+                            {date || ''}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-
-
     );
 };
 
--- Readio/src/pages/user/MyLibraryPage.jsx
+++ Readio/src/pages/user/MyLibraryPage.jsx
@@ -1,12 +1,14 @@
 import ProfileSection from '../../components/mylibrary/ProfileSection';
 import BookmarkSection from "../../components/mylibrary/BookmarkSection.jsx";
 import CalendarSection from "../../components/mylibrary/CalendarSection.jsx";
+import ReportSection from "../../components/mylibrary/ReportSection.jsx"
 const MyLibraryPage = () => {
     return (
         <div>
             <ProfileSection />
             <BookmarkSection />
             <CalendarSection />
+            <ReportSection />
         </div>
     );
 };
