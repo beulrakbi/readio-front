642679bf88c4cb4818ea60e20fc2ed9e6415ac6d
BBB924 <qhrud4231@naver.com>
2025-05-10 04:47:33 +0900

    #15 feat(join-complete): 회원가입 완료 페이지 UI 구현 완료

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
 import UserMain from "./pages/user/UserMain";
 import PostWriting from "./pages/post/PostWriting";
+import JoinComplete from "./pages/user/JoinComplete";
 
 function App() {
 
@@ -9,10 +10,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout/>}>
-          <Route index element={<UserMain/>}/>
-          <Route path="post/writing" element={<PostWriting/>}/>
-        </Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<UserMain />} />
+            <Route path="post/writing" element={<PostWriting />} />
+            <Route path="/join/complete" element={<JoinComplete />} /> {/* 회원가입 완료 */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
