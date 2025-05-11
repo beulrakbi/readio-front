f5fe431e8db8abfad0b31e487d676c1323820af7
LeeSang-In <nodt12@gmail.com>
2025-05-11 16:39:09 +0900

    #10 5.11 postDetail 2

--- Readio/src/pages/post/PostDetail.jsx
+++ Readio/src/pages/post/PostDetail.jsx
@@ -1,6 +1,9 @@
 import profileTory from '../../assets/profileTory.png';
 import postDetailOption from '../../assets/postDetailOption.png';
 import postLike from '../../assets/postLike.png';
+import postContentImg from '../../assets/postContentImg.png';
+import postDetailHeart from '../../assets/postDetailHeart.png';
+import postDetailReview from '../../assets/postDetailReview.png';
 import PostCSS from './Post.module.css';
 
 function PostDetail () {
@@ -22,8 +25,13 @@ function PostDetail () {
                     </button>
                 </div>
             </div> 
-            <h2>진짜 생존 신고</h2>
-            <p>어우 학교 못갈수도 있어요</p>
+            <h2 className={PostCSS.postDetailTitle}>진짜 생존 신고</h2>
+            <p className={PostCSS.postDetailContent}>어우 학교 못갈수도 있어요</p>
+            <img src={postContentImg} className={PostCSS.postContentImg}/>
+            <div className={PostCSS.postDetailHeartDiv}>
+                <span><img src={postDetailHeart} className={PostCSS.postDetailHeart}/>15</span>
+                <span><img src={postDetailReview} className={PostCSS.postDetailReview}/>3</span>
+            </div>
         </div>
     )
 }
