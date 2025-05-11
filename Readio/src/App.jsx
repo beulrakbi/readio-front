ca02b03b60c00e3aa447034a89070ffcf0cc8212
BBB924 <qhrud4231@naver.com>
2025-05-12 03:38:08 +0900

    #20 feat(find-account): 아이디/비밀번호 찾기 페이지 UI 구현 완료

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
 import UserMain from "./pages/user/UserMain";
 import PostWriting from "./pages/post/PostWriting";
+import FindAccount, { FindIdForm, FindPwdForm } from "./pages/user/FindAccount";
 
 function App() {
 
@@ -12,6 +13,11 @@ function App() {
           <Route path="/" element={<Layout/>}>
           <Route index element={<UserMain/>}/>
           <Route path="post/writing" element={<PostWriting/>}/>
+        <Route path="/find-account" element={<FindAccount />}>
+          <Route index element={<Navigate to="find-id" replace />} />
+          <Route path="find-id" element={<FindIdForm />} />
+          <Route path="find-pwd" element={<FindPwdForm />} />
+          </Route>
         </Route>
         </Routes>
       </BrowserRouter>
