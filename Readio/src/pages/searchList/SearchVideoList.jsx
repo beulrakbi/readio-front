c3f513e4b4789c223e9bf40b851b67561917c910
soomin0927 <ehfvls0927@naver.com>
2025-05-12 12:51:52 +0900

    #5. Header 검색 부분 코드 수정 / UserFooter Css 수정

--- Readio/src/pages/searchList/SearchVideoList.jsx
+++ Readio/src/pages/searchList/SearchVideoList.jsx
@@ -57,6 +57,16 @@ function SearchVideoList() {
                                    <div className={styles.videoDetail}>이적의 단어들 📚 은 싱어송라이터 이적이 시의 형식으로 쓴 에세이입니다. 짧게 단편으로 ...</div>
                               </div>
                          </div>
+                         <hr />
+
+                         <div className={styles.videoList}>
+                              <div className={styles.video}></div>
+                              <div className={styles.videoInfo}>
+                                   <div className={styles.videoTitle}>[이적의 단어들]📜 작가 이적. 책리뷰. 추천도서...</div>
+                                   <div className={styles.videoDate}>2023.08.05</div>
+                                   <div className={styles.videoDetail}>이적의 단어들 📚 은 싱어송라이터 이적이 시의 형식으로 쓴 에세이입니다. 짧게 단편으로 ...</div>
+                              </div>
+                         </div>
 
                     </div>
                     <hr />
