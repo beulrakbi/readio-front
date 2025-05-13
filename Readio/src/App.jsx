b1b1bdd78b5d014d571ffd82eaf7fb5f591dcfa6
BBB924 <qhrud4231@naver.com>
2025-05-13 11:01:50 +0900

    #33 feat(user-delete-complete): 회원탈퇴 완료 페이지 UI 구현 완료, 헤더 jsx, style 수정

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
-import UserMain from "./pages/user/UserMain";
 import PostWriting from "./pages/post/PostWriting";
+import UserDeleteComplete from "./pages/user/UserDeleteComplete";
+import UserMain from "./pages/user/UserMain";
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
           <Route path="/" element={<Layout/>}>
           <Route index element={<UserMain/>}/>
           <Route path="post/writing" element={<PostWriting/>}/>
+
+          <Route path="users/delete/complete" element={<UserDeleteComplete/>} />
         </Route>
         </Routes>
       </BrowserRouter>
