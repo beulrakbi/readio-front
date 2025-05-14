bda8612fbb335e7e2ff54dce0fa6f070fa1bb637
BBB924 <qhrud4231@naver.com>
2025-05-14 17:14:33 +0900

    (#4) refactor: 회원가입 페이지 jsx 구조 변경 및 스타일 수정, 파일 삭제
     - App.jsx: 로그인 코드 삭제
     - Header.module.css: 헤더 로고 정중앙 스타일 추가
     - Join.jsx: 유지보수 용이를 위해 회원정보 수정 페이지와 일치한 구조로 수정
     - join.module.css: 상동_스타일 수정
     - remove(Login.jsx,  Login.module.css): join 브랜치이므로 불필요한 파일 삭제

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,8 +1,6 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
 import Join from "./pages/user/Join";
-import Login from "./pages/user/Login";
 import UserMain from "./pages/user/UserMain";
 
 function App() {
@@ -13,8 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout/>}>
           <Route index element={<UserMain/>}/>
-        <Route path="/login" element={<Login />} /> {/* 로그인 */}
-        <Route path="/join" element={<Join />} /> {/* 회원가입 */}
+
+        <Route path="join" element={<Join />} /> {/* 회원가입 */}
           </Route>
         </Routes>
       </BrowserRouter>
