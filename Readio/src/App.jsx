bf52fd903e69349c88a9f46c84d17c769ab42cca
BBB924 <qhrud4231@naver.com>
2025-05-15 15:13:07 +0900

    #5 fix: App.jsx에서 /login 경로 앞 슬래시 제거

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout/>}>
           <Route index element={<UserMain/>}/>
-        <Route path="/login" element={<Login />} /> {/* 로그인 */}
+        <Route path="login" element={<Login />} /> {/* 로그인 */}
           </Route>
         </Routes>
       </BrowserRouter>
