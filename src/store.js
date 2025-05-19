c20c8e496634eae04f0a3120f68b28a7ed73d785
lurufarm <125882359+lurufarm@users.noreply.github.com>
2025-05-19 11:45:47 +0900

    test

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import bookSearchReducer from './components/modules/postwriting/bookSearchSlice';
-
-export const store = configureStore({
-    reducer: {
-        bookSearch: bookSearchReducer,
-    },
-});
