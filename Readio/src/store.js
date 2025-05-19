9b7a4fa4baac0f41c5e976818adbf6864e5cd232
BBB924 <qhrud4231@naver.com>
2025-05-20 01:24:16 +0900

    store ignore 설정 추가

--- Readio/src/store.js
+++ Readio/src/store.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducers from './modules/index.js';
+import rootReducer from './modules';
+import { composeWithDevTools} from '@redux-devtools/extension'
+import { createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
 
-const store = configureStore ({
-    reducer: rootReducers,
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-    devTools: true, // 개발환경이면 true, 배포 시 false로 바꿔도 됨
-}
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
