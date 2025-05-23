94628fc29cd99ca533148a815d04fced1e3d59e8
soomin0927 <ehfvls0927@naver.com>
2025-05-23 10:03:26 +0900

    - test/sm-master-searchList 생성

--- Readio/src/pages/searchList/SearchBookList.jsx
+++ Readio/src/pages/searchList/SearchBookList.jsx
@@ -91,20 +91,6 @@ function SearchBookList() {
                              <hr />
 
 
-                             <div className={styles.bookList}>
-                                  <div className={styles.book}></div>
-                                  <div className={styles.bookInfo}>
-                                       <div className={styles.bookTitle}>그리고 바통은 넘겨졌다.</div>
-                                       <div className={styles.credits}>
-                                            <div className={styles.bookAuthor}>세오 마이코</div>
-                                            <div className={styles.bookPublisher}>스토리텔러</div>
-                                       </div>
-                                       <div className={styles.bookDetail}>“부모 역할에 대해 생각해 보게 하는 소설”이 소설의 재미는 
-                                                                            기둥 줄거리를 이끌어 가는 37세 아버지와 17세 딸이 각각 아버지 역할과
-                                                                            딸 역할에 최선을 다하면서 만들어내는 미묘한 분위기에 있다....   
-                                                                        </div>
-                                  </div>
-                             </div>
     
      
                         </div>
