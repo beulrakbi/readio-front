4bd4af6fd7ed5c1a9a28e33832f58e2c66543f40
lurufarm <hsg200528@gmail.com>
2025-05-26 14:34:50 +0900

    #25 영상리스트 프론트 0526 sg

--- Readio/src/components/common/AdminNavbar.jsx
+++ Readio/src/components/common/AdminNavbar.jsx
@@ -1,37 +1,37 @@
-import logo from "../../assets/img/MainPageNavLogo.png";
-import styles from '../common/AdminNavbar.module.css';
+import { Link } from "react-router-dom";
+import logo from "../../assets/MainPageNavLogo.png";
+import styles from './AdminNavbar.module.css';
 
 function AdminNavbar() {
      return (
           <div className={styles.Navbar}>
-               <div className={styles.Nav}>
+               <div className={styles.buttonBox}>
+
                     <img src={logo} className={styles.Logo} alt="Logo" /> 
                     <br />
-                    <button className={styles.login}>로그인</button>
-                    <hr />
-                    <ul>회원 관리
-                         <li>회원 목록</li>
-                    </ul>
-                    <hr />
-                    <ul>신고 관리
-                         <li>리뷰</li>
-                         <li>포스트</li>
-                    </ul>
-                    <hr />
-                    <ul>게시판 관리
-                         <li>공지사항</li>
-                         <li>소식</li>
-                         <li>FAQ</li>
-                         <li>Q&A</li>
-                    </ul>
-                    <hr />
-                    <ul>사이트 관리
-                         <li>영상 필터링 / 키워드</li>
-                         <li>관심분야</li>
-                    </ul>
+                    {/* <button className={styles.login}>로그인</button> */}
+                    <Link to="/login" className={styles.login}>로그인</Link>
                </div>
-               <div className={styles.NavbarBox}></div>
-
+               <div className={styles.Nav}>
+                    <hr className={styles.adminNavBarHr} />
+                    <p className={styles.text1}>회원 관리</p>
+                    <Link to="/admin/users/list" className={styles.text2}>• 회원 목록</Link>
+                    <hr className={styles.adminNavBarHr} />
+                    <p className={styles.text1}>신고 관리</p>
+                    <Link to="/admin/reported/review/list" className={styles.text2}>• 리뷰</Link>
+                    <Link to="/admin/reported/post/list" className={styles.text2}>• 포스트</Link>
+                    <hr className={styles.adminNavBarHr} />
+                    <p className={styles.text1}>게시판 관리</p>
+                    <Link to="/admin/notice" className={styles.text2}>• 공지사항</Link>
+                    <Link to="/" className={styles.text2}>• 소식</Link>
+                    <Link to="/admin/faq" className={styles.text2}>• FAQ</Link>
+                    <Link to="/admin/qna" className={styles.text2}>• Q&A</Link>
+                    <hr className={styles.adminNavBarHr} />
+                    <p className={styles.text1}>사이트 관리</p>
+                    <Link to="/admin/filtering" className={styles.text2}>• 영상 필터링 / 키워드</Link>
+                    <Link to="/admin" className={styles.text2}>• 영상 키워드 관리</Link>
+                    <Link to="/admin/interest" className={styles.text2}>• 관심분야</Link>
+               </div>
           </div>
      );
 }
