c3f965fcb06d5b7c1e2124c97600e1dbe24428e5
BBB924 <qhrud4231@naver.com>
2025-05-26 16:17:06 +0900

    commit

--- Readio/src/main.jsx
+++ Readio/src/main.jsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client'; // createRoot를 직접 import
-import { Provider } from 'react-redux';
 import App from './App.jsx';
-import store from './Store.js';
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-);
+    <App />
+)
