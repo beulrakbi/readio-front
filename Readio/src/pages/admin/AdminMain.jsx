c1fe5a5888f5f755f1692b778f72c97d335fe159
BBB924 <qhrud4231@naver.com>
2025-05-28 03:27:22 +0900

    Feat: jwt 토큰 헤더에 자동 추가 처리 #87
    - 관리자 메인 적용완료

--- Readio/src/pages/admin/AdminMain.jsx
+++ Readio/src/pages/admin/AdminMain.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../apis/axiosInstance';
 import CurrentState from '../../components/adminmain/CurrentState';
 import Events from '../../components/adminmain/Events';
 import TopBooks from '../../components/adminmain/TopBooks';
@@ -5,21 +7,36 @@ import TopInterestsKeyword from '../../components/adminmain/TopInterestsKeywords
 import TopVideos from '../../components/adminmain/TopVideos';
 import AdminMainCSS from './AdminMain.module.css';
 
-function AdminMain()
-{
+function AdminMain() {
+
+    
+    // 공통적으로 axios 인스턴스를 사용하여 Authorization 헤더(토큰)를 자동 추가함
+    // 필요 시 axiosInstance.get('/api/endpoint')로 요청하시면 됩니다.
+    // 페이지 안 나올시 import 확인해보세요
+    // ----------------------- 시작
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        axiosInstance.get('/admin')
+            .then(response => setUserData(response.data))
+            .catch(error => console.error(error))
+    }, []);
+
+    // ----------------------- 끝
+
     return (
         <>
             <div className={AdminMainCSS.main}>
                 <div className={AdminMainCSS.container}>
-                    <CurrentState/>
-                    <Events/>
+                    <CurrentState />
+                    <Events />
                 </div>
                 <div className={AdminMainCSS.container}>
-                    <TopVideos/>
-                    <TopBooks/>
+                    <TopVideos />
+                    <TopBooks />
                 </div>
                 <div className={AdminMainCSS.container}>
-                    <TopInterestsKeyword/>
+                    <TopInterestsKeyword />
                 </div>
             </div>
         </>
