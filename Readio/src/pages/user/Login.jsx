a409037518b7adbb23981971b231b82c9b5ab674
BBB924 <qhrud4231@naver.com>
2025-05-28 13:05:28 +0900

    로그인 응답 정상, axios 토큰저장 정상, 403 에러 발생 해결중

--- Readio/src/pages/user/Login.jsx
+++ Readio/src/pages/user/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axiosInstance from '../../apis/axiosInstance';
 import loginImage from '../../assets/login.png';
 import LoginCSS from './Login.module.css';
 
@@ -24,44 +25,37 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:8080/users/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(formData),
-                credentials: "include" // 쿠키를 포함하여 요청
-            });
+            const response = await axiosInstance.post("/users/login", formData);
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message || "로그인 실패");
-            }
 
-            const data = await response.json();
+            // if (!response.ok) {
+            //     const errorData = response.data;
+            //     throw new Error(errorData.message || "로그인 실패");
+            // }
 
+            // const data = await response.json();
+            const data = response.data
             console.log("로그인 응답 data:", data);
 
-            localStorage.setItem("accessToken", data.accessToken);
+            localStorage.setItem("jwtToken", data.accessToken);
             localStorage.setItem("userId", data.userId); // 로그인한 사용자 ID 저장
             localStorage.setItem("userName", data.userName); // 로그인한 사용자 이름 저장
             localStorage.setItem("isPasswordVerified", "true"); // 비밀번호 검증 플래그 설정
 
             // window.location.href = "/";
 
-            const userInfoResponse = await fetch("http://localhost:8080/users/me", {
-                headers: {
-                    "Authorization": `Bearer ${data.accessToken}`
-                },
-                credentials: "include"
-            });
+            const token = localStorage.getItem("jwtToken");
+            console.log("axios 토큰 확인:", token); // 토큰이 null이면 저장이 안된것 배포전 삭제필요
 
-            if (!userInfoResponse.ok) {
-                throw new Error("사용자 정보 조회 실패");
-            }
+            // 토큰 저장 후 사용자 정보 조회
+            const userInfoResponse = await axiosInstance.get("/users/me");
 
-            const userInfo = await userInfoResponse.json();
+            const userInfo = userInfoResponse.data;
             console.log("userInfo:", userInfo);
+
+            // if (userInfoResponse.status !== 200) {
+            //     throw new Error("사용자 정보 조회 실패");
+            // }
 
             const roles = userInfo.userRole || []; // roles: ["USER"], ["ADMIN"], ["SUSPENDED"]
             console.log("userInfo.role:", userInfo.userRole)
@@ -76,7 +70,7 @@ const Login = () => {
             }
 
         } catch (error) {
-            alert(error.message || "로그인에 실패했습니다.");
+            alert(error.response?.data?.message || "로그인에 실패했습니다.");
             console.log("로그인 에러:", error);
         }
         console.log(formData);
