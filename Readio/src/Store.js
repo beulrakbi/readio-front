8ca02fad05a2d60b68b76aef3a0faad347c1c5a2
lurufarm <hsg200528@gmail.com>
2025-05-28 16:23:22 +0900

    master

---
+++ Readio/src/Store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducers from './modules/index.js';
+
+const store = configureStore ({
+    reducer: rootReducers,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    devTools: true, // 개발환경이면 true, 배포 시 false로 바꿔도 됨
+}
+);
+
+export default store;
