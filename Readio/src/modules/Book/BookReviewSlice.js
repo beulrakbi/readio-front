5b5bbdc748978b377bb4e0f5181d636b371225e5
lurufarm <hsg200528@gmail.com>
2025-05-30 13:12:00 +0900

    #14 책 상세페이지 리뷰 출력 완료

---
+++ Readio/src/modules/Book/BookReviewSlice.js
@@ -0,0 +1,26 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    reviews: [],
+    status: null,
+    message: null
+};
+
+
+const BookReviewSlice = createSlice({
+    name: 'bookReview',
+    initialState,
+    reducers: {
+        getBookReviews: (state, action) => {
+            state.reviews = action.payload.data;
+            state.status = action.payload.status;
+            state.message = action.payload.message;
+        }
+    }
+});
+
+export const {
+    getBookReviews
+} = BookReviewSlice.actions;
+
+export default BookReviewSlice.reducer;
