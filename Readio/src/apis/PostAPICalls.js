6aa80bcddda702cd292c3be07f930943bb301dd9
BBB924 <qhrud4231@naver.com>
2025-05-31 09:52:35 +0900

    Feat: 토큰 적용  #115
    - postAPICalls: sessionStorage 토큰으로 변경
    - PostReviewAPICalls: sessionStorage 토큰으로 변경
    - ReportedReviewAPICalss: 토큰 적용

--- Readio/src/apis/PostAPICalls.js
+++ Readio/src/apis/PostAPICalls.js
@@ -1,32 +1,32 @@
 import {
 	GET_POST,
-    POST_POST
+	POST_POST
 } from '../modules/postwriting/PostModule.js';
 
 const getAuthHeader = () => {
-const token = localStorage.getItem('accessToken'); // Login.jsx에서 저장한 토큰 키 이름과 일치하는지 확인!
-console.log("PostAPICall 토큰 :",  token)
-return token ? { 'Authorization': `Bearer ${token}` } : {};
+	const token = sessionStorage.getItem('accessToken'); // Login.jsx에서 저장한 토큰 키 이름과 일치하는지 확인!
+	console.log("PostAPICalls 토큰 :", token)
+	return token ? { 'Authorization': `Bearer ${token}` } : {};
 };
 
 export const callPostCreateAPI = ({ form }) => {
 	console.log('[PostAPICalls] callPostAPI Call');
 
-     // 👇 VITE_APP_RESTAPI_IP 값이 제대로 들어오는지 확인!
-    console.log('VITE_APP_RESTAPI_IP:', import.meta.env.VITE_APP_RESTAPI_IP); 
-	
+	// 👇 VITE_APP_RESTAPI_IP 값이 제대로 들어오는지 확인!
+	console.log('VITE_APP_RESTAPI_IP:', import.meta.env.VITE_APP_RESTAPI_IP);
+
 	const requestURL = `http://localhost:8080/mylibrary/post/writing`;
 
-        // 👇 실제로 만들어진 URL 확인!
-    console.log('Request URL:', requestURL); 
-console.log("VITE_APP_RESTAPI_IP 환경 변수 값:", import.meta.env.VITE_APP_RESTAPI_IP); // ✨ 환경 변수 값도 확인!
+	// 👇 실제로 만들어진 URL 확인!
+	console.log('Request URL:', requestURL);
+	console.log("VITE_APP_RESTAPI_IP 환경 변수 값:", import.meta.env.VITE_APP_RESTAPI_IP); // ✨ 환경 변수 값도 확인!
 
 	return async (dispatch, getState) => {
 		const result = await fetch(requestURL, {
 			method: 'POST',
 			headers: {
 				Accept: '*/*',
-				...getAuthHeader()
+				...getAuthHeader
 			},
 			body: form
 		}).then((response) => response.json());
@@ -40,8 +40,8 @@ console.log("VITE_APP_RESTAPI_IP 환경 변수 값:", import.meta.env.VITE_APP_R
 export const callPostDetailAPI = ({ postId }) => {
 	const requestURL = `http://localhost:8080/mylibrary/post/${postId}`;
 
-	        // 👇 실제로 만들어진 URL 확인!
-    console.log('Request URL:', requestURL); 
+	// 👇 실제로 만들어진 URL 확인!
+	console.log('Request URL:', requestURL);
 	console.log("VITE_APP_RESTAPI_IP 환경 변수 값:", import.meta.env.VITE_APP_RESTAPI_IP); // ✨ 환경 변수 값도 확인!
 
 	return async (dispatch, getState) => {
