fa5115d5921e6671c8facfdd408a83b66aed5a4e
BBB924 <qhrud4231@naver.com>
2025-06-05 11:05:19 +0900

    UserNav css 수정
    - 메뉴클릭 시 자동으로 닫히게끔 수정

--- Readio/src/components/common/UserNav.jsx
+++ Readio/src/components/common/UserNav.jsx
@@ -1,21 +1,23 @@
 import { useSelector } from "react-redux";
-import { NavLink , useNavigate} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import UserNavCSS from './navi.module.css';
 
 
-function UserNav({ isOpen }) {
+function UserNav({ isOpen, setIsOpen }) {
 
     const isLogin = useSelector(state => state.user.isLogin);
-    // console.log('로그인상태', useSelector(state => state));
 
-    const navigate = useNavigate();
-    const handleLibraryClick = () => {
-        if (isLogin) {
-            navigate('/mylibrary');
-        } else {
-            navigate('/guestlibrary');
-        }
-    };
+    // 삭제 예정
+    // const navigate = useNavigate();
+
+    // const handleLibraryClick = () => {
+    //     if (isLogin) {
+    //         navigate('/mylibrary');
+    //     } else {
+    //         navigate('/guestlibrary');
+    //     }
+    //     setIsOpen(false);
+    // };
 
     return (
         <div className={`${UserNavCSS.navi} ${isOpen ? UserNavCSS.open : ''}`}>
@@ -26,26 +28,66 @@ function UserNav({ isOpen }) {
                 </div>
                 <div className={UserNavCSS.naviLink}>
                     {isLogin && (
-                        <NavLink to="/users/verifypwd" className={UserNavCSS.naviLinkText}>내 정보 수정</NavLink>
+                        <NavLink to="/users/verifypwd"
+                            className={UserNavCSS.naviLinkText}
+                            onClick={() => setIsOpen(false)}
+                        >내 정보 수정</NavLink>
                     )}
-                    <div
+
+                    <NavLink
+                        to={isLogin ? "/mylibrary" : "/guestlibrary"}
+                        className={UserNavCSS.naviLinkText}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        내 서재
+                    </NavLink>
+
+                    {/* <div 삭제 예정
                         onClick={handleLibraryClick}
                         className={UserNavCSS.naviLinkText}
                         style={{ cursor: 'pointer' }}
                     >
                         내 서재
-                    </div>
-                    <NavLink to="/" className={UserNavCSS.naviLinkText}>피드</NavLink>
-                    <NavLink to="/" className={UserNavCSS.naviLinkText}>소식</NavLink>
-                    <ul style={{ fontSize: '20px', paddingInlineStart: '0px' }}>고객센터
-                        <li className={UserNavCSS.naviLinkText}><NavLink to="/notice" className={UserNavCSS.naviLinkText}>공지사항</NavLink></li>
-                        <li className={UserNavCSS.naviLinkText}><NavLink to="/faq" className={UserNavCSS.naviLinkText}>FAQ</NavLink></li>
-                        <li className={UserNavCSS.naviLinkText}><NavLink to="/qna" className={UserNavCSS.naviLinkText}>Q&A</NavLink></li>
+                    </div> */}
+
+                    <NavLink
+                        to="/"
+                        className={UserNavCSS.naviLinkText}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        피드
+                    </NavLink>
+
+                    <NavLink
+                        to="/"
+                        className={UserNavCSS.naviLinkText}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        소식
+                    </NavLink>
+
+                    <ul style={{ fontSize: '20px', paddingInlineStart: '0px' }}>
+                        고객센터
+                        <li className={UserNavCSS.naviLinkText}>
+                            <NavLink to="/notice" className={UserNavCSS.naviLinkText}
+                                onClick={() => setIsOpen(false)}
+                            >공지사항
+                            </NavLink></li>
+                        <li className={UserNavCSS.naviLinkText}>
+                            <NavLink to="/faq" className={UserNavCSS.naviLinkText}
+                                onClick={() => setIsOpen(false)}
+                            >FAQ
+                            </NavLink></li>
+                        <li className={UserNavCSS.naviLinkText}>
+                            <NavLink to="/qna" className={UserNavCSS.naviLinkText}
+                                onClick={() => setIsOpen(false)}
+                            >Q&A
+                            </NavLink></li>
                     </ul>
                 </div>
                 <div className={UserNavCSS.naviBannerContainer}>
                     <p className={UserNavCSS.naviBannerText}>오늘의 소식</p>
-                    <NavLink to="/">
+                    <NavLink to="/" onClick={() => setIsOpen(false)}>
                         <div className={UserNavCSS.naviBanner}>
                             <img alt="오늘의 소식 이미지" />
                         </div>
